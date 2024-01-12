e407101dfa5e7a74f2976e5fddbcb2caf0c396ef
mmwandera <mmwandera74@gmail.com>
2024-01-12 11:17:10 +0300

    completed assignment

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,5 @@
-def parrot():
-    pass
+#The parrot() function should accept an argument of a string and both print() and return the string at the end of the function.
+#The parrot() function should have a default argument of "Squawk!"
+def parrot(string="Squawk!"):
+    print(string)
+    return string
